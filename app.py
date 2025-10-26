b29afa4fb682573bff8d168850f947fa51cf592b
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2025-10-26 15:24:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-# Flask setup with imports, routes, and all website pages:
+# Flask setup- imports, routes, and all website pages:
 
 from flask import Flask, render_template, request
+from models import get_db_connection
 
 app = Flask(__name__)
 
@@ -9,20 +10,45 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-# load about page-
+# about page-
 @app.route('/macros')
 def macros():
     return render_template('macros.html')
 
-# open sign in-
-@app.route('/register')
+# sign in-
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO users (email, password) VALUES (?, ?)", (email, password))
+        conn.commit()
+        conn.close()
+        message = "Account Created!"
+        return render_template('register.html', message=message)
     return render_template('register.html')
 
-# open log in
-@app.route('/login')
+# log in-
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM users WHERE email=? AND password=?", (email, password))
+        user = cur.fetchone()
+        conn.close()
+        if user:
+            message = "You are Logged In"
+        else:
+            message = "Oh No, Invalid!"
+        return render_template('login.html', message=message)
     return render_template('login.html')
+
+
 
 # GET and POST using log page so users can log food and see log history
 
@@ -33,6 +59,7 @@ def log():
     if request.method == 'POST':
         meal = request.form['meal']
         calories = request.form['calories']
+        
          # Print in term and show on screen the meal user just logged:
         message = f"Meal logged: {meal} ({calories} calories)"
         print(message)
