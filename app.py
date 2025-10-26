7133c16b80ab352f7a76ee3cb0f8218415b22891
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2025-10-26 14:22:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Flask setup with imports, routes, and all website pages:
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -20,13 +20,22 @@ def register():
 def login():
     return render_template('login.html')
 
-@app.route('/log')
-def log():
-    return render_template('log.html')
-
 @app.route('/logs')
 def logs():
     return render_template('logs.html')
+
+# GET and POST using log page:
+
+@app.route('/log', methods=['GET', 'POST'])
+def log():
+    if request.method == 'POST':
+        meal = request.form['meal']
+        calories = request.form['calories']
+         # Print and show meal user just logged:
+        message = f"Meal logged: {meal} ({calories} calories)"
+        print(message)  # Still shows in the terminal too
+        return render_template('log.html', message=message)
+    return render_template('log.html')
 
 
 # When I run python app.py, go ahead and start the website server so I can see my pages:
