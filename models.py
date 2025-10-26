07d2e61ff247c00418656a1d9dda5c1e4c9e54d0
Jade Dofat <143310236+jdofat@users.noreply.github.com>
2025-10-26 14:38:03 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,19 @@
+# SQLite import, database connection, start the setup, create tables (users: id, email, pass) and meals (id, user id, meal, cals), and save changes/close connection
 
+import sqlite3
+
+# connects to the database file-
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    return conn
+
+# builds 2 tables (users & meals)-
+def init_db():
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, password TEXT)')
+    cur.execute('CREATE TABLE IF NOT EXISTS meals (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, meal TEXT, calories INTEGER, FOREIGN KEY (user_id) REFERENCES users (id))')
+
+    conn.commit()
+    conn.close()
